52eee7f92c7823051162fab3b702683c76567bda
pszczepkowski <p.szczepkowski87@gmail.com>
2023-08-17 16:59:55 +0200

    Initial commit

---
+++ src/main/java/pl/com/coders/libria1/Libria1Application.java
@@ -0,0 +1,13 @@
+package pl.com.coders.libria1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Libria1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Libria1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/com/coders/libria1/Libria1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.coders.libria1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Libria1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
